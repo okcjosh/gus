a2022475f77580bed00db6f42e3114a594ca9efd
okcjosh <krauselwitz@me.com>
2016-11-17 14:55:25 -0600

    Adding more Braintree API bullshit

--- client/app/checkout/show/show.component.js
+++ client/app/checkout/show/show.component.js
@@ -1,90 +1,22 @@
 'use strict';
 const angular = require('angular');
+
 const uiRouter = require('angular-ui-router');
+
 import routes from './show.routes';
 
-export function BrainsComponent(config) {
-  this.config = config;
-  this.config.development = config.development || false;
-
-  this.paymentForm = $('#' + config.formID);
-  this.inputs = $('input[type=text], input[type=email], input[type=tel]');
-  this.button = this.paymentForm.find('.button');
-
-  this.states = {
-    show: 'active',
-    wait: 'loading'
-  };
-  this.focusClass = 'has-focus';
-  this.valueClass = 'has-value';
-
-  this.initialize();
+export class ShowComponent {
+  /*@ngInject*/
+  constructor() {
+    this.message = 'Hello';
+  }
 }
 
-BrainsComponent.prototype.initialize = function () {
-  const self = this;
-
-  this.events();
-  this.inputs.each(function (index, element) {
-    self.labelHander($(element));
-  });
-  this.notify('error');
-};
-
-BrainsComponent.prototype.events = function () {
-  const self = this;
-
-  this.inputs.on('focus', function () {
-    $(this).closest('label').addClass(self.focusClass);
-    self.labelHander($(this));
-  }).on('keydown', function () {
-    self.labelHander($(this));
-  }).on('blur', function () {
-    $(this).closest('label').removeClass(self.focusClass);
-    self.labelHander($(this));
-  });
-};
-
-BrainsComponent.prototype.labelHander = function (element) {
-  const self = this;
-  const input = element;
-  const label = input.closest('label');
-
-  window.setTimeout(function () {
-    const hasValue = input.val().length > 0;
-
-    if (hasValue) {
-      label.addClass(self.valueClass);
-    } else {
-      label.removeClass(self.valueClass);
-    }
-  }, 10);
-};
-
-BrainsComponent.prototype.notify = function (status) {
-  const self = this;
-  const notice = $('.notice-' + status);
-  const delay = this.config.development === true ? 4000 : 2000;
-
-  notice.show();
-
-  window.setTimeout(function () {
-    notice.addClass('show');
-    self.button.removeClass(self.states.wait);
-
-    window.setTimeout(function () {
-      notice.removeClass('show');
-      window.setTimeout(function () {
-        notice.hide();
-      }, 310);
-    }, delay);
-  }, 10);
-};
 export default angular.module('gusApp.show', [uiRouter])
   .config(routes)
   .component('show', {
     template: require('./show.html'),
-    controller: BrainsComponent,
-    controllerAs: 'brainsCtrl'
+    controller: ShowComponent,
+    controllerAs: 'showCtrl'
   })
   .name;
--- server/routes.js
+++ server/routes.js
@@ -6,7 +6,66 @@
 
 import errors from './components/errors';
 import path from 'path';
-import gateway, {formatErrors, createResultObject} from './gateway';
+app.use('/api/leos', require('./api/leo'));
+app.use('/api/events', require('./api/event'));
+app.use('/api/things', require('./api/thing'));
+app.use('/api/users', require('./api/user'));
+app.use('/auth', require('./auth').default);
+let braintree = require('braintree');
+let environment, gateway;
+require('dotenv').load();
+environment = process.env.BT_ENVIRONMENT.charAt(0).toUpperCase() + process.env.BT_ENVIRONMENT.slice(1);
+
+gateway = braintree.connect({
+  environment: braintree.Environment[environment],
+  merchantId: process.env.BT_MERCHANT_ID,
+  publicKey: process.env.BT_PUBLIC_KEY,
+  privateKey: process.env.BT_PRIVATE_KEY
+});
+
+
+export let TRANSACTION_SUCCESS_STATUSES = [
+  braintree.Transaction.Status.Authorizing,
+  braintree.Transaction.Status.Authorized,
+  braintree.Transaction.Status.Settled,
+  braintree.Transaction.Status.Settling,
+  braintree.Transaction.Status.SettlementConfirmed,
+  braintree.Transaction.Status.SettlementPending,
+  braintree.Transaction.Status.SubmittedForSettlement
+];
+
+
+export function formatErrors(errors) {
+  let formattedErrors = '';
+
+  for (let i in errors) { // eslint-disable-line no-inner-declarations, lets-on-top
+    if (errors.hasOwnProperty(i)) {
+      formattedErrors += 'Error: ' + errors[i].code + ': ' + errors[i].message + '\n';
+    }
+  }
+  return formattedErrors;
+}
+
+export function createResultObject(transaction) {
+  let result;
+  let status = transaction.status;
+
+  if (TRANSACTION_SUCCESS_STATUSES.indexOf(status) !== -1) {
+    result = {
+      header: 'Sweet Success!',
+      icon: 'success',
+      message: 'Your test transaction has been successfully processed. See the Braintree API response and try again.'
+    };
+  } else {
+    result = {
+      header: 'Transaction Failed',
+      icon: 'fail',
+      message: 'Your test transaction has a status of ' + status + '. See the Braintree API response and try again.'
+    };
+  }
+
+  return result;
+}
 
 export default function(app) {
   // Insert routes below
@@ -22,11 +81,7 @@ export default function(app) {
   // app.use('/api/dept_preferences', require('./api/dept_preference'));
   // app.use('/api/departments', require('./api/department'));
   // app.use('/api/def_dept_preferences', require('./api/def_dept_preference'));
-  app.use('/api/leos', require('./api/leo'));
-  app.use('/api/events', require('./api/event'));
-  app.use('/api/things', require('./api/thing'));
-  app.use('/api/users', require('./api/user'));
-  app.use('/auth', require('./auth').default);
+
 
 
   // All undefined asset or api routes should return a 404
