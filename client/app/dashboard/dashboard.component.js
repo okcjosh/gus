0b41a89353ffdb8306de41a711b821f0acd6e0c1
okcjosh <krauselwitz@me.com>
2016-12-04 17:35:06 -0600

    Client Route Invitation

--- client/app/dashboard/dashboard.component.js
+++ client/app/dashboard/dashboard.component.js
@@ -4,13 +4,82 @@ const angular = require('angular');
 const uiRouter = require('angular-ui-router');
 
 import routes from './dashboard.routes';
-
+const jquery = require('jquery');
 export class DashboardComponent {
   /*@ngInject*/
   constructor() {
     this.message = 'Hello';
   }
 }
+  let handleDataTableButtons = function () {
+    if ($("#datatable-buttons").length) {
+      $("#datatable-buttons").DataTable({
+        dom: "Bfrtip",
+        buttons: [
+          {
+            extend: "copy",
+            className: "btn-sm"
+          },
+          {
+            extend: "csv",
+            className: "btn-sm"
+          },
+          {
+            extend: "excel",
+            className: "btn-sm"
+          },
+          {
+            extend: "pdfHtml5",
+            className: "btn-sm"
+          },
+          {
+            extend: "print",
+            className: "btn-sm"
+          },
+        ],
+        responsive: true
+      });
+    }
+
+
+    let TableManageButtons;
+    TableManageButtons = function () {
+      return {
+        init: function () {
+          handleDataTableButtons();
+        }
+      };
+    }();
+    $('#datatable').dataTable();
+    $('#datatable-keytable').DataTable({
+      keys: true
+    });
+    $('#datatable-responsive').DataTable();
+    $('#datatable-scroller').DataTable({
+      ajax: "js/datatables/json/scroller-demo.json",
+      deferRender: true,
+      scrollY: 380,
+      scrollCollapse: true,
+      scroller: true
+    });
+    $('#datatable-fixed-header').DataTable({
+      fixedHeader: true
+    });
+    let $datatable = $('#datatable-checkbox');
+    $datatable.dataTable({
+      'order': [[1, 'asc']],
+      'columnDefs': [
+        {orderable: false, targets: [0]}
+      ]
+    });
+    $datatable.on('draw.dt', function () {
+      $('input').iCheck({
+        checkboxClass: 'icheckbox_flat-green'
+      });
+    });
+    TableManageButtons.init();
+  };
+
 
 export default angular.module('gusApp.dashboard', [uiRouter])
   .config(routes)
