9976768d7c2f55291749ccd7e3947c52c74ab549
okcjosh <krauselwitz@me.com>
2016-12-17 13:32:35 -0600

    85 ft 0 bubble

--- client/app/event/event.component.js
+++ client/app/event/event.component.js
@@ -1,9 +1,9 @@
 'use strict';
 const angular = require('angular');
+const $ = require('jquery');
+const jquery = require('jquery');
 const uiRouter = require('angular-ui-router');
 import routing from './event.routes';
-const bootstrap = require('bootstrap');
-
 export class EventComponent {
   awesomeEvents = [];
   eventTitle = '';
@@ -48,6 +48,75 @@ export class EventComponent {
         this.awesomeEvents = response.data;
         this.socket.syncUpdates('event', this.awesomeEvents);
       });
+
+
+    $(document).ready(function () {
+      /** ******************************
+       * Alert Message Boxes
+       ****************************** **/
+      $('.msgClose').click(function (e) {
+        e.preventDefault();
+        $(this).closest('.alertMsg').fadeOut("slow", function () {
+          $(this).addClass('hidden');
+        });
+      });
+
+      /** ******************************
+       * Activate Tool-tips
+       ****************************** **/
+      // $("[data-toggle='tooltip']").tooltip();
+
+      /** ******************************
+       * Activate Popovers
+       ****************************** **/
+      // $("[data-toggle='popover']").popover();
+
+      /** ******************************
+       * Form Placeholders
+       ****************************** **/
+      let placehold = {
+        init: function () {
+          $('input[type="text"], input[type="email"], input[type="password"], textarea').each(placehold.replace);
+        },
+        replace: function () {
+          let txt = $(this).data('placeholder');
+          if (txt) {
+            if ($(this).val() == '') {
+              $(this).val(txt);
+            }
+            $(this).focus(function () {
+              if ($(this).val() == txt) {
+                $(this).val('');
+              }
+            }).blur(function () {
+              if ($(this).val() == '') {
+                $(this).val(txt);
+              }
+            });
+          }
+        }
+      }
+      placehold.init();
+
+      /** ******************************
+       * Required Fields
+       ****************************** **/
+      $("form :input[required='required']").blur(function () {
+        if (!$(this).val()) {
+          $(this).addClass('hasError');
+        } else {
+          if ($(this).hasClass('hasError')) {
+            $(this).removeClass('hasError');
+          }
+        }
+      });
+      $("form :input[required='required']").change(function () {
+        if ($(this).hasClass('hasError')) {
+          $(this).removeClass('hasError');
+        }
+      });
+
+    });
   }
 
   addEvent($state) {
