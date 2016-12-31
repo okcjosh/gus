9acb1d94ae18fbf29fe6c270eb38953356762534
okcjosh <krauselwitz@me.com>
2016-12-30 22:42:30 -0600

    85 ft 0 bubble

--- client/app/main/main.component.js
+++ client/app/main/main.component.js
@@ -1,16 +1,32 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import routing from './main.routes';
+const jquery = require('jquery');
+let $ = require( 'jquery' );
+require( 'datatables.net' );
+require( 'datatables.net-buttons');
+require( 'datatables.net-buttons-bs');
+require( 'datatables.net-bs');
+require( 'datatables.net-buttons-bs');
+require( 'datatables.net-fixedheader');
+//require( 'datatables.net-fixedheader-bs');
+require( 'datatables.net-keytable');
+require( 'datatables.net-responsive');
+require( 'datatables.net-responsive-bs');
+require( 'datatables.net-select');
+require( 'datatables.net-scroller');
+//require( 'datatables.net-scroller-bs');
 
 export class MainController {
-  awesomeThings = [];
-  newThing = '';
 
   /*@ngInject*/
-  constructor($http, $scope, socket, Auth) {
+  constructor($http, $scope, socket, $state, $interpolate, Auth) {
     this.$http = $http;
     this.socket = socket;
     this.isLoggedIn = Auth.isLoggedInSync;
+    this.$scope = $scope;
+    this.$scope.$state = $state;
+    this.$interpolate = $interpolate;
 
     $scope.$on('$destroy', function() {
       socket.unsyncUpdates('thing');
@@ -18,12 +34,31 @@ export class MainController {
   }
 
   $onInit() {
-    this.$http.get('/api/things')
+    this.$http.get('/api/events')
       .then(response => {
-        this.awesomeThings = response.data;
-        this.socket.syncUpdates('thing', this.awesomeThings);
-      });
-  }
+        this.dataSet = response.data;
+        // alert(this.dataSet[0].name);
+        let table =  $('#example').DataTable( {
+          data: this.dataSet,
+          columns: [
+            {data: "event_id", title: "ID", visible: false},
+            {data: "venue", title: "Venue Data", visible: false},
+            {data: "address", title: "Location"},
+            {data: "phone_number", title: "Phone Number"},
+            {data: "point_of_contact", title: "POC"},
+            {data: "status_id", title: "Status"},
+            {data: "job_type", title: "Job Type", visible: false},
+            {data: "job_type_specs", title: "Job Specs", visible: false},
+            {data: "prefered_officer_name", title: "Prefered Officer", visible: false},
+            {data: "is_recuring", title: "Is Recuring", visible: false},
+            {data: "recuring_data", title: "Recuring", visible: false},
+            {data: "date", title: "Date", visible: false},
+            // { data: "status", title: "Status" },
+            // { data: "event_type", title: "Event Type" }
+            ]
+          });
+        })
+      }
 
   addThing() {
     if(this.newThing) {
@@ -58,3 +93,13 @@ export default angular.module('es4App.main', [uiRouter])
     controller: MainController
   })
   .name;
+
+
+
+
+
+
+
+
+
+
