1c5ade2ae62d9630d1925aa6bd2ae316649206b4
okcjosh <krauselwitz@me.com>
2017-01-08 12:02:42 -0600

    85 ft 0 bubble

--- client/app/dashboard/dashboard.component.js
+++ client/app/dashboard/dashboard.component.js
@@ -76,6 +76,27 @@ export class DashboardComponent {
 
         let table = $('#events').DataTable({
           select: true,
+          initComplete: function () {
+            this.api().columns().every(function () {
+              var column = this;
+              var select = $('<select><option value=""></option></select>')
+                .appendTo($(column.header()).empty())
+                .on('change', function () {
+                  var val = $.fn.dataTable.util.escapeRegex(
+                    $(this).val()
+                  );
+
+                  column
+                    .search(val ? '^' + val + '$' : '', true, false)
+                    .draw();
+                });
+
+              column.data().unique().sort().each(function (d, j) {
+                select.append('<option value="' + d + '">' + d + '</option>')
+              });
+            });
+          },
+
           data: this.dataSet,
           columns: [
             {data: "_id", title: "ID", visible: false},
