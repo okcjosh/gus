3b215d945ce55fff66f665642066fdd8ca0b84c8
Joshua Johnson <krauselwitz@me.com>
2017-03-15 20:37:33 -0500

    balls

---
+++ server/braintree_transaction.js
@@ -0,0 +1,31 @@
+/**
+ * Created by joshuajohnson on 3/15/17.
+ */
+'use strict';
+
+let braintree = require('braintree');
+let environment, gateway;
+let serviceFeeAmount = 11 * .25;
+
+gateway = braintree.connect({
+  environment: braintree.Environment.Sandbox,
+  merchantId: 'swvg9scjkhfhq9rs',
+  publicKey: '78ghksfzt5z5hfcx',
+  privateKey: '1f210164c4fff82b6da4c29131f30379'
+});
+
+module.exports = gateway;
+
+
+gateway.transaction.sale({
+  merchantAccountId: "blue_ladders_store",
+  amount: "1999.00",
+  paymentMethodNonce: "fake-valid-nonce",
+  serviceFeeAmount: "serviceFeeAmount"
+}, function (err, result) {
+  console.log(result);
+  console.log(serviceFeeAmount)
+
+
+
+});
