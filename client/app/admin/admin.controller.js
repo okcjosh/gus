4e3db38ff90eb1af80ec4678eef26e50d9c56850
Joshua Johnson <krauselwitz@me.com>
2017-03-21 12:47:14 -0500

    balls

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -92,6 +92,62 @@ export default class AdminController {
       this.accountNumber = '';
       this.routingNumber = '';
     }
+
+    let braintree = require('braintree');
+    let environment, gateway;
+
+    gateway = braintree.connect({
+      environment: braintree.Environment.Sandbox,
+      merchantId: 'swvg9scjkhfhq9rs',
+      publicKey: '78ghksfzt5z5hfcx',
+      privateKey: '1f210164c4fff82b6da4c29131f30379'
+    });
+
+    module.exports = gateway;
+
+    var merchantAccountParams;
+    merchantAccountParams = {
+      individual: {
+        firstName: "Jane",
+        lastName: "Doe",
+        email: "jane@14ladders.com",
+        phone: "5553334444",
+        dateOfBirth: "1981-11-19",
+        ssn: "456-45-4567",
+        address: {
+          streetAddress: "111 Main St",
+          locality: "Chicago",
+          region: "IL",
+          postalCode: "60622"
+        }
+      },
+      business: {
+        legalName: "Jane's Ladders",
+        dbaName: "Jane's Ladders",
+        taxId: "98-7654321",
+        address: {
+          streetAddress: "111 Main St",
+          locality: "Chicago",
+          region: "IL",
+          postalCode: "60622"
+        }
+      },
+      funding: {
+        descriptor: "Blue Ladders",
+        destination: braintree.MerchantAccount.FundingDestination.Bank,
+        email: "funding@blueladders.com",
+        mobilePhone: "5555555555",
+        accountNumber: "1123581321",
+        routingNumber: "071101307"
+      },
+      tosAccepted: true,
+      masterMerchantAccountId: "americanhustlersyndicate",
+      id: "blue_ladders_store",
+    };
+
+    gateway.merchantAccount.create(merchantAccountParams, function (err, result) {
+      console.log(result)
+    });
   }
 
   deleteLeo(leo) {
